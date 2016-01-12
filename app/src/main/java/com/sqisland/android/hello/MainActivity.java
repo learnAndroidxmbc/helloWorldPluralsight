ac76574e5c820bff0a35ec7ca53247e05ca8e712
ernie <xbmcsetup3000@gmail.com>
2016-01-11 21:43:49 -0500

    Updated with additons with Menu item to say Bye...

--- app/src/main/java/com/sqisland/android/hello/MainActivity.java
+++ app/src/main/java/com/sqisland/android/hello/MainActivity.java
@@ -2,11 +2,43 @@ package com.sqisland.android.hello;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+  private TextView greetingView;
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    greetingView = (TextView) findViewById(R.id.greeting);
+  }
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    // Replace super.onCreate call
+    //   return super.onCreateOptionsMenu(menu);
+
+    // need to inflate our own menu
+
+
+    // get Inflator for Menu
+    getMenuInflater().inflate( R.menu.activity_main, menu);
+
+    return (true);
+
+
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+
+    if (item.getItemId() == R.id.action_bye){
+      greetingView.setText(R.string.bye);
+    }
+    return super.onOptionsItemSelected(item);
   }
 }
