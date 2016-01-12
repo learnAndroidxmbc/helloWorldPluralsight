9d22ad8e2196bcdc380403df229ac1d789d859df
ernie <xbmcsetup3000@gmail.com>
2016-01-12 16:55:02 -0500

    Espresso test not working 100%

--- app/src/androidTest/java/com/sqisland/android/hello/MainActivityTest.java
+++ app/src/androidTest/java/com/sqisland/android/hello/MainActivityTest.java
@@ -1,5 +1,6 @@
 package com.sqisland.android.hello;
 
+import android.support.test.InstrumentationRegistry;
 import android.support.test.espresso.ViewInteraction;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
@@ -9,9 +10,11 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.action.ViewActions.click;
 
 /**
  * Created by ernie on 1/12/2016.
@@ -23,8 +26,17 @@ public class MainActivityTest {
 
     @Test
     public void bye() {
+
+        //  check if the   R.id.greeting  matches with  Hello World
         ViewInteraction check = onView(withId(R.id.greeting))
                 .check(matches(withText(R.string.hello_world)));
+
+        openActionBarOverflowOrOptionsMenu(InstrumentationRegistry.getTargetContext());
+        onView(withText(R.string.say_bye))
+                .perform(click());
+
+        onView(withId(R.id.action_bye))
+            .check(matches(withText(R.string.hello_world)));
     }
 }
 
